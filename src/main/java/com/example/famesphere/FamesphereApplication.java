fc1173c137179ac1d8efe7e9017565319b0612fb
vaibhavdikha2206 <vaibhavdikha@gmail.com>
2023-07-29 23:59:19 +0530

    Added Comments

--- src/main/java/com/example/famesphere/FamesphereApplication.java
+++ src/main/java/com/example/famesphere/FamesphereApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class FamesphereApplication {
 
+	// Famesphere
 	public static void main(String[] args) {
 		SpringApplication.run(FamesphereApplication.class, args);
 	}
